e022b3911b2b5339f27ea185ea01108ab865791a
Yufei HU <mhu6729@gmail.com>
2022-05-05 17:24:00 +0200

    update

--- NAO_Seg_deeplabv3plus/train_NAO_deeplab_cityscapes.sh
+++ NAO_Seg_deeplabv3plus/train_NAO_deeplab_cityscapes.sh
@@ -6,17 +6,16 @@ DATA_DIR=data
 mkdir -p $OUTPUT_DIR
 
 # with mor seed0 100-30
-fixed_arc="0 5 0 5 2 3 1 1 3 1 2 6 4 2 2 3 0 5 4 2"
+#fixed_arc="0 5 0 5 2 3 1 1 3 1 2 6 4 2 2 3 0 5 4 2"
 # with mor seed1 150-50
 fixed_arc="1 2 1 3 1 5 1 5 3 3 2 4 3 2 3 4 2 4 1 4"
 
 CUDA_VISIBLE_DEVICES=0,1 python train_NAO_deeplabv3plus.py \
-  --data=$DATA_DIR \
-  --data_root "/home/student/workspace_Yufei/CityScapes/leftImg8bit/" \
+  --data_root "/home/student/workspace_Yufei/CityScapes/NAO_Cityscapes" \
   --arch="$fixed_arc" \
   --model "deeplabv3plus_resnet50"\
   --output_stride 8 \
-  --batch_size 16 \
+  --batch_size 8 \
   --crop_size 768 \
   --gpu_id 0,1 \
   --lr 0.1 \
