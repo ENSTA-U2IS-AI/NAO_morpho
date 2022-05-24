54364bb9e218d43ff38baf25338b0dd49656b6b2
YufeiHU-fr <56996070+YufeiHU-fr@users.noreply.github.com>
2022-05-24 16:38:57 +0200

    nao morph

--- NAO_Seg_deeplabv3plus/train_search_NAO_deeplabv3plus.sh
+++
@@ -1,9 +0,0 @@
-MODEL=search_NAO_deeplab
-OUTPUT_DIR=exp/$MODEL
-
-mkdir -p $OUTPUT_DIR
-
-CUDA_VISIBLE_DEVICES=0 python train_NAO_deeplabv3plus_search.py --search_space with_mor_ops\
-    --seed 0\
-    --output_dir=$OUTPUT_DIR | tee -a $OUTPUT_DIR/train.log \
-    > train_deeplab_search.out
